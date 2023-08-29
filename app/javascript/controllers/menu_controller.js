a675e99de877f7178dbf66601a1cb50bf7ba496c
Antonio Quaresma <tozequa@gmail.com>
2023-08-29 18:03:46 +0100

    build menu bar but missing links

---
+++ app/javascript/controllers/menu_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="menu"
+export default class extends Controller {
+  static targets = ["tab"];
+
+  connect() {
+    this.activeTab = null; // Track the currently active tab
+
+    // Initialize the first tab as active
+    this.activateTab(this.tabTargets[0]);
+  }
+
+  toggleTab(event) {
+    event.preventDefault();
+    const clickedTab = event.currentTarget;
+
+    if (clickedTab !== this.activeTab) {
+      this.deactivateTab(this.activeTab);
+      this.activateTab(clickedTab);
+    }
+  }
+
+  activateTab(tab) {
+    tab.classList.add("active");
+    this.activeTab = tab;
+  }
+
+  deactivateTab(tab) {
+    tab.classList.remove("active");
+  }
+}
