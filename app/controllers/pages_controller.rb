3d9c52ddc44902172a040526e626e87a244321b3
Antonio Quaresma <tozequa@gmail.com>
2023-08-31 17:25:11 +0100

    building logIn/signIn/signOut/account

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
+
   def home
+    @user = current_user
+  end
+
+  def account
+    @user = current_user
   end
 
   def dashboard
