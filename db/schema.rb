422d04e33f8682646ceec74c4205ba1f27dd13ce
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-04 16:11:51 +0100

    photography on the account page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_113506) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_150731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -113,6 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_113506) do
     t.string "first_name"
     t.string "last_name"
     t.string "role"
+    t.string "photography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
