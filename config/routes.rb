abe069181d5bbb67ac7194487dadb09d5ef0e2a3
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-04 11:46:45 +0100

    family_members routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get 'dashboard', to: 'pages#dashboard'
   get 'account', to: 'pages#account'
+
   resources :residents do
     resources :reports, only: [:index, :create]
+    resources :family_members, only: [:new, :create, :edit, :update, :show, :index]
   end
+
   resources :reports, only: [:show] do
     resources :report_categories, only: [:new, :index, :create, :show]
   end
