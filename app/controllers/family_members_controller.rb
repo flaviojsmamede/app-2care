b4d5c341f9d83037017efdd80ac02c849ceee0f8
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-04 13:05:43 +0100

    index for family members without link on the card

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -1,10 +1,16 @@
 class FamilyMembersController < ApplicationController
   def index
     @resident = Resident.find(params[:resident_id])
-    @family_members = FamilyMember.all
+    @family_members = @resident.family_members
   end
 
   def show
     @family_member = FamilyMember.find(params[:id])
+  end
+
+  private
+
+  def family_member_params
+    params.require(:family_member).permit(:first_name, :last_name, :phone_number, :email, :address, :relationship, :resident_id)
   end
 end
