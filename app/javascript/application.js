8c1862586eefe283b45224e52684232b6ebd9d2b
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-06 12:17:06 +0100

    solved alert import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-
-import AlertController from "./controllers/alert_controller.js"
-Stimulus.register("alert", AlertController)
