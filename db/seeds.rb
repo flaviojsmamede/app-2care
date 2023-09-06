690c97fc0182d80f5df3be21de43a43b649c95b6
Ana Sofia Correia <asofiac215@gmail.com>
2023-09-06 18:02:25 +0100

    created sweet alerts informations and link them to each report category

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Cleaning DB..."
+ReportCategory.destroy_all
+puts "ReportCategories deleted!"
 Option.destroy_all
 puts "Options deleted!"
 Category.destroy_all
@@ -22,43 +24,43 @@ puts "Users deleted!"
 
 puts "Creating new DB..."
 
-category1 = Category.create(name: "sleep", icon: '<i class="fa-solid fa-moon"></i>')
+category1 = Category.create(name: "sleep", icon: "<i class='fa-solid fa-moon'></i>")
 
 option1 = Option.create(name: "good", category: category1)
 option2 = Option.create(name: "medium", category: category1)
 option3 = Option.create(name: "bad", category: category1)
 
-category2 = Category.create(name: "eat", icon: '<i class="fa-solid fa-utensils"></i>')
+category2 = Category.create(name: "eat", icon: "<i class='fa-solid fa-utensils'></i>")
 
 option4 = Option.create(name: "good", category: category2)
 option5 =Option.create(name: "medium", category: category2)
 option6 =Option.create(name: "bad", category: category2)
 
-category3 = Category.create(name: "humour", icon: '<i class="fa-solid fa-masks-theater"></i>')
+category3 = Category.create(name: "humour", icon: "<i class='fa-solid fa-masks-theater'></i>")
 
 option7 = Option.create(name: "good", category: category3)
 option8 = Option.create(name: "medium", category: category3)
 option9 = Option.create(name: "bad", category: category3)
 
-category4 = Category.create(name: "activities", icon: '<i class="fa-solid fa-person-running"></i>')
+category4 = Category.create(name: "activities", icon: "<i class='fa-solid fa-person-running'></i>")
 
 option10 = Option.create(name: "yoga", category: category4)
 option11 = Option.create(name: "chess", category: category4)
 option12 = Option.create(name: "walk", category: category4)
 
-category5 = Category.create(name: "blood pressure", icon: '<i class="fa-solid fa-heart"></i>')
+category5 = Category.create(name: "blood pressure", icon: "<i class='fa-solid fa-heart'></i>")
 
 option14 = Option.create(name: "high", category: category5)
 option15 = Option.create(name: "good", category: category5)
 option16 = Option.create(name: "low", category: category5)
 
-category6 = Category.create(name: "medication", icon: '<i class="fa-solid fa-pills"></i>')
+category6 = Category.create(name: "medication", icon: "<i class='fa-solid fa-pills'></i>")
 
 option17 = Option.create(name: "all", category: category6)
 option18 = Option.create(name: "partial", category: category6)
 option19 = Option.create(name: "none", category: category6)
 
-category7 = Category.create(name: "hygiene", icon: '<i class="fa-solid fa-shower"></i>')
+category7 = Category.create(name: "hygiene", icon: "<i class='fa-solid fa-shower'></i>")
 
 option20 = Option.create(name: "good", category: category7)
 option21 = Option.create(name: "medium", category: category7)
