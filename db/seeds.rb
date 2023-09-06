5a5e44809b890741d1469e67bc42dd2361159eb3
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-06 16:57:27 +0100

    more residents

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,15 +69,16 @@ puts "Categories created!"
 ############
 
 # Create the "Antonio" family member outside of the loop
-antonio = FamilyMember.create(
-  first_name: "Antonio",
-  last_name: "Quaresma",
-  phone_number: "966666666",
-  email: "tozequa@gmail.com",
-  address: "Rua da Alegria, 123",
-  relationship: "son",
-  photography: "residents/antonio.png"
-)
+# antonio = FamilyMember.create(
+#   first_name: "Antonio",
+#   last_name: "Quaresma",
+#   phone_number: "966666666",
+#   email: "tozequa@gmail.com",
+#   address: "Rua da Alegria, 123",
+#   # resident_id: resident.id,
+#   relationship: "son",
+#   photography: "residents/antonio.png"
+# )
 
 names = [
   "Pedro Agostinho",
@@ -134,7 +135,7 @@ names.each_with_index do |full_name, i|
   )
 
   # Create a resident associated with the care taker and the "Antonio" family member
-  resident = Resident.create(
+  Resident.create(
     user_id: care_taker.id,
     first_name: first_name,
     last_name: last_name,
@@ -144,82 +145,89 @@ names.each_with_index do |full_name, i|
   )
 
   # Associate the "Antonio" family member with the resident
-  resident.family_members << antonio
+  # resident.family_members << antonio
 end
 
+care_taker14 = User.create(email: "flavio2@gmail.com", password: "123456", first_name: "flavio", last_name: "mamede", role: "care taker")
+resident15 = Resident.create(user_id: care_taker14.id, first_name: "Rita", last_name: "Laia", birth_date: "15/04/1963", clinical_info: "Insomnia", photography: "residents/rita.png")
+family_member1 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident15.id, photography: "residents/antonio.png")
+
+care_taker13 = User.create(email: "flavio1@gmail.com", password: "123456", first_name: "flavio", last_name: "mamede", role: "care taker")
+resident16 = Resident.create(user_id: care_taker13.id, first_name: "samuel", last_name: "ven", birth_date: "15/04/1963", clinical_info: "Insomnia", photography: "residents/samuel.png")
+family_member1 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident16.id, photography: "residents/antonio.png")
 
 care_taker1 = User.create(email: "flavio@gmail.com", password: "123456", first_name: "flavio", last_name: "mamede", role: "care taker")
-resident1 = Resident.create(user_id: care_taker1.id, first_name: "nuno", last_name: "cassandra", birth_date: "15/04/1963", clinical_info: "Insomnia", photography: "residents/nuno.png")
-# report1 = Report.create(resident_id: resident1.id)
+resident17 = Resident.create(user_id: care_taker1.id, first_name: "nuno", last_name: "cassandra", birth_date: "15/04/1963", clinical_info: "Migraine", photography: "residents/nuno.png")
+# report1 = Report.create(resident_id: resident17.id)
 # report_category1 = ReportCategory.create(category: category4, report: report1, option: option10)
-family_member1 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident1.id, photography: "residents/antonio.png")
+family_member1 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident17.id, photography: "residents/antonio.png")
 
 care_taker2 = User.create(email: "ana@gmail.com", password: "123456", first_name: "ana", last_name: "almeida", role: "care taker")
-resident2 = Resident.create(user_id: care_taker2.id, first_name: "armindo", last_name: "matias", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/armindo.png")
-# report2 = Report.create(resident_id: resident2.id)
+resident18 = Resident.create(user_id: care_taker2.id, first_name: "armindo", last_name: "matias", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/armindo.png")
+# report2 = Report.create(resident_id: resident18.id)
 # report_category2 = ReportCategory.create(category: category1, report: report2, option: option1)
-family_member2 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident2.id, photography: "residents/antonio.png")
+family_member2 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident18.id, photography: "residents/antonio.png")
 
 care_taker3 = User.create(email: "nuno@gmail.com", password: "123456", first_name: "flavio", last_name: "cassandro", role: "care taker")
-resident3 = Resident.create(user_id: care_taker3.id, first_name: "luana", last_name: "pereira", birth_date: "15/04/1963", clinical_info: "allergies", photography: "residents/luana.png")
+resident19 = Resident.create(user_id: care_taker3.id, first_name: "luana", last_name: "pereira", birth_date: "15/04/1963", clinical_info: "allergies", photography: "residents/luana.png")
 # report3 = Report.create(resident_id: resident3.id)
 # report_category3 = ReportCategory.create(category: category5, report: report3, option: option16)
-family_member3 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident3.id, photography: "residents/antonio.png")
+family_member3 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident19.id, photography: "residents/antonio.png")
 
 care_taker4 = User.create(email: "rodrigo@gmail.com", password: "123456", first_name: "flavio", last_name: "coelho", role: "care taker")
-resident4 = Resident.create(user_id: care_taker4.id, first_name: "antonio", last_name: "quaresma", birth_date: "15/04/1963", clinical_info: "blind", photography: "residents/antonio.png")
-# report4 = Report.create(resident_id: resident4.id)
+resident20 = Resident.create(user_id: care_taker4.id, first_name: "antonio", last_name: "quaresma", birth_date: "15/04/1963", clinical_info: "blind", photography: "residents/antonio.png")
+# report4 = Report.create(resident_id: resident20.id)
 # report_category3 = ReportCategory.create(category: category7, report: report4, option: option20)
-family_member4 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident4.id, photography: "residents/antonio.png")
+family_member4 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident20.id, photography: "residents/antonio.png")
 
 care_taker5 = User.create(email: "julia@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident5 = Resident.create(user_id: care_taker5.id, first_name: "margarida", last_name: "almeida", birth_date: "15/04/1963", clinical_info: "deaf", photography: "residents/margarida.png")
-# report5 = Report.create(resident_id: resident5.id)
+resident21 = Resident.create(user_id: care_taker5.id, first_name: "margarida", last_name: "almeida", birth_date: "15/04/1963", clinical_info: "deaf", photography: "residents/margarida.png")
+# report5 = Report.create(resident_id: resident21.id)
 # report_category3 = ReportCategory.create(category: category3, report: report5, option: option20)
-family_member5 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident5.id, photography: "residents/antonio.png")
+family_member5 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident21.id, photography: "residents/antonio.png")
 
 care_taker6 = User.create(email: "julia1@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident6 = Resident.create(user_id: care_taker6.id, first_name: "beatriz", last_name: "vieira", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/beatriz.png")
-# report6 = Report.create(resident_id: resident6.id)
+resident22 = Resident.create(user_id: care_taker6.id, first_name: "beatriz", last_name: "vieira", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/beatriz.png")
+# report6 = Report.create(resident_id: resident22.id)
 # report_category4 = ReportCategory.create(category: category4, report: report6, option: option20)
-family_member6 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident6.id, photography: "residents/antonio.png")
+family_member6 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident22.id, photography: "residents/antonio.png")
 
 
 care_taker7 = User.create(email: "julia2@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident7 = Resident.create(user_id: care_taker7.id, first_name: "flavio", last_name: "mamede", birth_date: "15/04/1963", clinical_info: "allergies", photography: "residents/flavio.png")
-# report7 = Report.create(resident_id: resident7.id)
+resident23 = Resident.create(user_id: care_taker7.id, first_name: "flavio", last_name: "mamede", birth_date: "15/04/1963", clinical_info: "allergies", photography: "residents/flavio.png")
+# report7 = Report.create(resident_id: resident23.id)
 # report_category5 = ReportCategory.create(category: category5, report: report7, option: option20)
-family_member7 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident7.id, photography: "residents/antonio.png")
+family_member7 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident23.id, photography: "residents/antonio.png")
 
 
 care_taker8 = User.create(email: "julia3@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident8 = Resident.create(user_id: care_taker8.id, first_name: "elidiane", last_name: "bezerra", birth_date: "15/04/1963", clinical_info: "back pain", photography: "residents/elidiane.png")
-# report8 = Report.create(resident_id: resident8.id)
+resident24 = Resident.create(user_id: care_taker8.id, first_name: "elidiane", last_name: "bezerra", birth_date: "15/04/1963", clinical_info: "back pain", photography: "residents/elidiane.png")
+# report8 = Report.create(resident_id: resident24.id)
 # report_category6 = ReportCategory.create(category: category6, report: report8, option: option20)
-family_member8 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident8.id, photography: "residents/antonio.png")
+family_member8 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident24.id, photography: "residents/antonio.png")
 
 care_taker9 = User.create(email: "julia4@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident9 = Resident.create(user_id: care_taker9.id, first_name: "kenneth", last_name: "stuen", birth_date: "15/04/1963", clinical_info: "deaf", photography: "residents/ken.png")
-# report9 = Report.create(resident_id: resident9.id)
+resident25 = Resident.create(user_id: care_taker9.id, first_name: "kenneth", last_name: "stuen", birth_date: "15/04/1963", clinical_info: "deaf", photography: "residents/ken.png")
+# report9 = Report.create(resident_id: resident25.id)
 # report_category7 = ReportCategory.create(category: category7, report: report9, option: option20)
-family_member9 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident9.id, photography: "residents/antonio.png")
+family_member9 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident25.id, photography: "residents/antonio.png")
 
 care_taker10 = User.create(email: "julia5@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident10 = Resident.create(user_id: care_taker10.id, first_name: "maria", last_name: "azfal", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/maria.png")
-# report10 = Report.create(resident_id: resident10.id)
+resident26 = Resident.create(user_id: care_taker10.id, first_name: "maria", last_name: "azfal", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/maria.png")
+# report10 = Report.create(resident_id: resident26.id)
 # report_category8 = ReportCategory.create(category: category7, report: report10, option: option20)
-family_member10 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident10.id, photography: "residents/antonio.png")
+family_member10 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident26.id, photography: "residents/antonio.png")
 
 care_taker11 = User.create(email: "julia6@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident11 = Resident.create(user_id: care_taker11.id, first_name: "rodrigo", last_name: "coelho", birth_date: "15/04/1963", clinical_info: "back pain", photography: "residents/rodrigo.png")
-# report11 = Report.create(resident_id: resident11.id)
+resident27 = Resident.create(user_id: care_taker11.id, first_name: "rodrigo", last_name: "coelho", birth_date: "15/04/1963", clinical_info: "back pain", photography: "residents/rodrigo.png")
+# report11 = Report.create(resident_id: resident27.id)
 # report_category9 = ReportCategory.create(category: category4, report: report11, option: option20)
-family_member11 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident11.id, photography: "residents/antonio.png")
+family_member11 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident27.id, photography: "residents/antonio.png")
 
 care_taker12 = User.create(email: "julia7@gmail.com", password: "123456", first_name: "julia", last_name: "coelho", role: "care taker")
-resident12 = Resident.create(user_id: care_taker12.id, first_name: "sofia", last_name: "correia", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/sofia.png")
-# report12 = Report.create(resident_id: resident12.id)
+resident28 = Resident.create(user_id: care_taker12.id, first_name: "sofia", last_name: "correia", birth_date: "15/04/1963", clinical_info: "diabetes", photography: "residents/sofia.png")
+# report12 = Report.create(resident_id: resident28.id)
 # report_category10 = ReportCategory.create(category: category6, report: report12, option: option20)
-family_member12 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident12.id, photography: "residents/antonio.png")
+family_member12 = FamilyMember.create(first_name: "antonio", last_name: "quaresma", phone_number: "966666666", email: "tozequa@gmail.com", address: "Rua da Alegria, 123", relationship: "son", resident_id: resident28.id, photography: "residents/antonio.png")
 
 puts "All done!"
