310fe6565d2607fa833aba48b9444516b0989702
Flavio Mamede <flaviojsmamede@gmail.com>
2023-09-07 11:31:35 +0100

    fixed the radio buttons click

--- app/javascript/controllers/options_controller.js
+++ app/javascript/controllers/options_controller.js
@@ -3,19 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="options"
 export default class extends Controller {
   static values = { index: String, url: String }
-  static targets = [ "optionBtn0", "optionBtn1", "optionBtn2", "selectBtn"]
+  static targets = [ "optionBtn0", "optionBtn1", "optionBtn2", "selectBtn", "fieldset"]
 
   connect() {
-    console.log(this.children)
   }
 
   select(event) {
     const childrenArray = Array.from(event.currentTarget.parentElement.children);
     childrenArray.forEach((element) => {
       if (element !== event.currentTarget) {
-        element.classList.remove(element.dataset.index)
+        element.classList.remove(element.dataset.index);
+        if(element.querySelector('input[type="radio"]')) {
+          element.querySelector('input[type="radio"]').checked = false;
+        }
       }else {
-        event.currentTarget.classList.add(event.currentTarget.dataset.index)
+        event.currentTarget.classList.add(event.currentTarget.dataset.index);
+        event.currentTarget.querySelector('input[type="radio"]').checked = true;
       }
     })
   }
